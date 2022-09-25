b8207d5e85050a2e63e73e097aff9f1edd56f62a
Sadath Syed <sadathsadu2002@gmail.com>
2022-09-25 21:44:02 +0530

    slide-fade

--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -52,7 +52,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Navigation } from "swiper";
+import { EffectFade, Navigation, Pagination } from "swiper";
 // import { bannerData } from "../../dummyData";
 import { motion } from "framer-motion"
 
@@ -63,24 +63,71 @@ const Hero = () => {
     }
     return (
         <motion.div
-            className='h-screen  overflow-hidden z-0'
+            className='h-screen  overflow-hidden z-0 !transition-all !ease-in-out !duration-300'
             initial="hidden"
             animate="visible"
             transition={{
-                delay: 0.2,
+                // duration:4,
                 x: { type: "spring", stiffness: 100 },
-                default: { duration: 0.7 },
+                default: { duration: 4 },
             }}
             variants={variants}
         >
             <Swiper
+                // navigation={true}
+                // modules={[Navigation]}
+                // className="mySwiper"
+                // effect={"fade"}
+
+                effect={"fade"}
                 navigation={true}
-                modules={[Navigation]}
-                className="mySwiper"
+                pagination={{
+                  clickable: true,
+                }}
+                modules={[EffectFade, Navigation, Pagination]}
+
             >
-                {[0,1,2,3,4].map((item, i) => (
+              
                     <SwiperSlide
-                        key={i}
+                        // key={i}
+                    >
+                        <div
+                            style={{
+                                backgroundImage: `linear-gradient(rgba(1,1,1,0.2),rgba(1,1,1,0.2) ), url(${"/images/house.jpg"})`
+                            }}
+                            className='!bg-cover flex items-center justify-center !bg-center !bg-fixed h-[90vh] md:h-[80vh]'
+                        >
+                            <div className="flex text-center flex-col space-y-4 lg:space-y-8">
+                                <h1
+                                    style={{
+                                        textShadow: '0 0 10px #111111'
+                                    }}
+                                    data-aos="fade-up"
+                                    data-aos-delay="300"
+                                    data-aos-duration="1000"
+                                    className="text-3xl md:text-5xl lg:text-7xl font-black text-white">BUILD CONFIDENCE!</h1>
+                                <p
+                                    data-aos="fade-up"
+                                    data-aos-delay="400"
+                                    data-aos-duration="1000"
+                                    className="text-white font-semibold text-lg max-w-[95%] mx-auto md:text-2xl lg:text-3xl">
+                                    Helping you to feel more confident both inside and outside 
+                                </p>
+
+                                <div
+                                    data-aos="fade-up"
+                                    data-aos-delay="500"
+                                    data-aos-duration="1000"
+                                    className="mx-auto">
+                                    {/* <Button text='Start now' /> */}
+                                    <button>Start New</button>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+
+                    <SwiperSlide
+                        // key={i}
                     >
                         <div
                             style={{
@@ -116,7 +163,7 @@ const Hero = () => {
                             </div>
                         </div>
                     </SwiperSlide>
-                ))}
+                
 
 
 
